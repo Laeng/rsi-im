9f2ff1fc851c0920a3b9e19f0739f4f1e894aba6
Laeng <Laeng@users.noreply.github.com>
2022-08-14 18:12:00 +0900

    Add vue3

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -1,1 +1,12 @@
-// Build something cool. <3
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { resolvePageComponent } from 'vite-plugin-laravel/inertia'
+
+createInertiaApp({
+	resolve: (name) => resolvePageComponent(name, import.meta.glob('../views/pages/**/*.vue')),
+	setup({ el, app, props, plugin }) {
+		createApp({ render: () => h(app, props) })
+			.use(plugin)
+			.mount(el)
+	},
+})
