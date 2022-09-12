f0e001feae93045bfda3915f42e94c9a08ff8ad1
Laeng <Laeng@users.noreply.github.com>
2022-09-12 13:24:48 +0900

    chore: add defaultHeading vue

---
+++ resources/views/components/heading/default-heading.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="md:flex md:items-center md:justify-between">
+        <div class="min-w-0 flex-1">
+            <h2 class="text-2xl font-bold dark:text-white leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                <slot name="title"/>
+            </h2>
+        </div>
+        <div class="mt-4 flex md:mt-0 md:ml-4">
+            <slot name="buttons"/>
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
