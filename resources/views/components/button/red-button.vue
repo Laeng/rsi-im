4d34d41f100b76f02ae1d92da4d7d145b588e888
Laeng <Laeng@users.noreply.github.com>
2022-09-15 22:53:51 +0900

    chore: add red button vue

---
+++ resources/views/components/button/red-button.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <button class="flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:enabled:bg-red-700 focus:outline-none focus:ring-1 focus:ring-red-500 disabled:opacity-80 disabled:cursor-not-allowed">
+        <slot />
+    </button>
+</template>
